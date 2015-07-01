9ef61c3f3c0dfcc600cfc95a1fb8e8b05860b7a9
Deivide (David) Oliveira <deivide.s.oliveira@gmail.com>
2015-07-01 22:53:45 +0000

    Add basic game logic

--- app.js
+++ app.js
@@ -1,5 +1,79 @@
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
+    playerCash = 100;
+
+    //Updates Player's cash each time it is called
+    var updatePlayerCash = function () {
+        $('.money').text(playerCash);
+    };
+
+    var showResults = function () {
+        $('.number-label').hide();
+        $('.btn-guess').hide();
+        $('.bet-label').show();
+        $('.btn-bet').show();
+        $('.score-label').show();  
+    };
+
+    //Compare the choices of the players
+    var compareResults = function(player1Choice, player2Choice) {
+        if (player1Choice === player2Choice) {
+            var win = playerBet * 2;
+            playerCash = playerCash + win;
+            updatePlayerCash();
+            $('<h2 class="message">').appendTo(".intro-body").text("Nice play! You won $" + win).css('color', 'green');
+            showResults();          
+            // alert("Computer choice was: " + player1Choice + ". Your choice was: " + player2Choice + ". Nice play! You won $" + win + ". Your cash is now $" + playerCash);
+
+        } else if (player1Choice === player2Choice -1 || player1Choice === player2Choice + 1) {
+            updatePlayerCash();
+            $('<h2 class="message">').appendTo(".intro-body").text("Almost! You will keep your bet.").css('color', 'orange');
+            showResults(); 
+            // alert("Computer choice was: " + player1Choice + ". Your choice was: " + player2Choice + ". Almost! You will keep your bet. Your cash is now $" + playerCash);
+        } else {
+            var lost = playerBet;
+            playerCash = playerCash - playerBet;
+            updatePlayerCash();
+            $('<h2 class="message">').appendTo(".intro-body").text("Wrong choice. You have lost $" + playerBet).css('color', 'red');
+            showResults(); 
+            // alert("Computer choice was: " + player1Choice + ". Your choice was: " + player2Choice + ". You have lost your bet! Your cash is now $" + playerCash);
+        }
+    }
+
+    var match = function() {
+        compareResults(computerChoice, playerChoice); 
+    }
+
+    $('.brand-heading').on('click', function() {
+        $(this).hide();
+        $('.btn-circle').hide();
+        $('.bet-label').show();
+        $('.btn-bet').show();
+        $('.score-label').show();
+    });
+
+    $('.btn-bet').on('click', function() {
+        if (playerCash > 0) {
+            playerBet = $(this).data("bet");
+            $('.btn-bet').hide();
+            $('.bet-label').hide();
+            $('.number-label').show();
+            $('.btn-guess').show();
+        } else {
+            alert("Game Over! You have lost all your money");
+            $('.btn-bet').hide();
+            $('.bet-label').hide();
+            $('.score-label').hide();
+            $('.brand-heading').show();
+        }
+    });
+
+    $('.btn-guess').on('click', function() {
+        playerChoice = $(this).data("guess");
+        computerChoice = Math.ceil(Math.random()*10);
+        match();
+    });
+
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
@@ -7,11 +81,7 @@ $(function() {
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
-    $('.brand-heading').on('click', function() {
-        $(this).hide();
-        $('.bet-label').show();
-        $('.btn-bet').show();
-    });
+
 });
 
 // Closes the Responsive Menu on Menu Item Click
@@ -27,47 +97,3 @@ $(window).scroll(function() {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
 });
-
-
-
-// (function(){
-//     playerCash = 100;
-
-//     var bet = function() {
-//         computerChoice = Math.ceil(Math.random()*10);
-//         playerBet = prompt("What is your bet, between $5 and $10?");
-//         while (playerBet < 5 || playerBet > 10) {
-//             alert("Invalid bet. Please try again");
-//             playerBet = prompt("What is your bet, between $5 and $10?");    
-//         }        
-//         playerChoice = prompt("Choose a number between 1 and 10:");
-//         while (playerChoice < 1 || playerChoice > 10) {
-//             alert("Invalid number. Please try again");
-//             playerChoice = prompt("Choose a number between 1 and 10:");
-//         }
-//     }
-
-//     var compareResults = function(player1Choice, player2Choice) {
-//         if (player1Choice === player2Choice) {
-//             var win = playerBet * 2;
-//             playerCash = playerCash + win;
-//             alert("Computer choice was: " + player1Choice + ". Your choice was: " + player2Choice + ". Nice play! You won $" + win + ". Your cash is now $" + playerCash);
-//         } else if (player1Choice === player2Choice -1 || player1Choice === player2Choice + 1) {
-//             alert("Computer choice was: " + player1Choice + ". Your choice was: " + player2Choice + ". Almost! You will keep your bet. Your cash is now $" + playerCash);
-//         } else {
-//             var lost = playerBet;
-//             playerCash = playerCash - playerBet;
-//             alert("Computer choice was: " + player1Choice + ". Your choice was: " + player2Choice + ". You have lost your bet! Your cash is now $" + playerCash);
-//         }
-//     }
-
-//     var match = function() {
-//         while (playerCash > 0) {
-//           bet();
-//           compareResults(computerChoice, playerChoice); 
-//         }
-//         alert("Game Over! You have lost all your money");
-//     }
-
-//     match();
-// }());
